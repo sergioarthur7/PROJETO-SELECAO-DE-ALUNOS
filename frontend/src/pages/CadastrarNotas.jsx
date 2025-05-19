c187a8afa98b6d6fcbc61e9eee139f8da33810d5
Sérgio <sergioarthurinfo2325@gmail.com>
2025-05-19 16:22:06 -0300

    V6.8

--- frontend/src/pages/CadastrarNotas.jsx
+++ frontend/src/pages/CadastrarNotas.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/CadastrarNotas.css'; // Certifique-se de que a folha de estilo existe e está correta
 
-const API_URL = import.meta.env.VITE_API_URL || "https://projeto-selecao.vercel.app";
+const API_URL = import.meta.env.VITE_API_URL || "https://projeto-selecao-de-alunos.vercel.app";
 
 const materias = [
   "Língua Portuguesa",
