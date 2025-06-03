ba9088bc46675b1e700627b021f148923c7c9593
Sérgio Arthur <eusergiosobral@gmail.com>
2025-06-03 18:10:11 -0300

    V7.1

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,15 @@
-const mysql = require("mysql");
-require("dotenv").config();
+// backend/db.js
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = mysql.createPool({
-  connectionLimit: 10, // Limite de conexões simultâneas
+  connectionLimit: 10,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
-module.exports = db;
+export default db;
