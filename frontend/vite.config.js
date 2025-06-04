1cb24631d9de1c712a106020d7901a45c3bc07da
Sergio <sergioarthurinfo2325@gmail.com>
2025-06-04 11:07:18 -0300

    V8.6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // Garante que CSS e assets funcionem no Vercel
+  base: './', // Garante que CSS e assets funcionem no Vercel
 
   // ⚠️ Esse proxy é só útil para desenvolvimento local.
   // Pode manter para testes locais, sem afetar Vercel.
