69bb9304917c5d1aeb089fc67e8b9e4e44c167d1
Sérgio Arthur <eusergiosobral@gmail.com>
2025-06-09 08:38:11 -0300

    V11.4

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,11 @@
-// 📁 backend/db.js
 import mysql from 'mysql2/promise';
 
 export default async function getConnection() {
   return await mysql.createConnection({
-    host: 'us-east.connect.psdb.io',
-    user: 'u981546547_kaio', // substitua pelo usuário gerado pelo PlanetScale
-    password: '8;KUYNI?We',  // substitua pela senha gerada
-    database: 'u981546547_selecao2', // nome do banco no PlanetScale
-    ssl: {
-      rejectUnauthorized: true, // 🔒 necessário para conexões seguras
-    },
+    host: '45.132.157.103', // IP do seu servidor Hostinger
+    user: 'u981546547_kaio',
+    password: '8;KUYNI?We',
+    database: 'u981546547_selecao2',
+    port: 3306,
   });
 }
